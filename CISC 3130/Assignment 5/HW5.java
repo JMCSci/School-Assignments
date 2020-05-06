3650c14bc5ea367852478cbe3490ed73abc31b9d
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-05-05 20:59:53 -0400

    Update HW5.java

--- CISC 3130/Assignment 5/HW5.java
+++ CISC 3130/Assignment 5/HW5.java
@@ -1,3 +1,11 @@
+/* Jason Moreau
+ * CISC 3130 - EWQ6
+ * Spring 2020
+ *
+ * Assignment #5 - More Trees (General Trees)
+ *
+ */
+
 package assignment5;
 
 import java.util.Scanner;
@@ -23,38 +31,46 @@ public class HW5 {
 		System.out.println("*** TREE 1 *** ");
 		System.out.print("Parent of Dan: " + tree1.getParent("Dan") + "\n");
 		System.out.print("Uncles of Jake: ");
-		tree1.getUncles("Jake");
-		System.out.print("Oldest child: ");
+		tree1.uncles("Jake");
+		System.out.print("\nOldest child of Jones: ");
 		tree1.oldestSon("Jones");
-		System.out.print("Youngest child: ");
+		System.out.print("Youngest child of Jones: ");
 		tree1.youngestSon("Jones");
+		System.out.print("Children of Deville: ");
+		tree1.children("Deville");
 		
 		// Tree 2
 		System.out.println("\n*** TREE 2 *** ");
 		System.out.print("Grandparent of Scott: ");
-		tree2.getGrandparent("Scott");
+		tree2.grandparent("Scott");
 		System.out.print("Siblings of Phillip: ");
 		tree2.getSibling("Phillip");
 		System.out.print("Children of Mabel: ");
-		tree2.getChildren("Mabel");
+		tree2.children("Mabel");
+		System.out.print("\nChildren of Jane: ");
+		tree2.children("Jane");
+		System.out.print("\nParent of Jane: ");
+		tree2.getParentName("Jane");
 		
 		// Tree 3
-		System.out.println("\n\n*** TREE 3 *** ");
+		System.out.println("\n*** TREE 3 *** ");
 		System.out.print("Children of Kelly: ");
-		tree3.getChildren("Kelly");
+		tree3.children("Kelly");
 		System.out.print("\nGrandparent of Kelly: ");
-		tree3.getGrandparent("Kelly");
-		System.out.print("\nOldest sibling of Sidney: ");
+		tree3.grandparent("Kelly");
+		System.out.print("Oldest sibling of Sidney: ");
 		tree3.oldestSibling("Sidney");
 		
 		// Tree 4
 		System.out.println("\n*** TREE 4 *** ");
 		System.out.print("Uncles of Isabel: ");
-		tree4.getUncles("Isabel");
-		System.out.print("Grandparent of Harper: ");
-		tree4.getGrandparent("Harper"); 
+		tree4.uncles("Isabel");
+		System.out.print("\nGrandparent of Harper: ");
+		tree4.grandparent("Harper"); 
 		System.out.print("Sibling of Harper: ");
 		tree4.getSibling("Harper");
+		System.out.print("Oldest son of Noah: ");
+		tree4.oldestSon("Noah");
 		
 		// Tree 5
 		System.out.println("\n*** TREE 5 *** ");
@@ -63,13 +79,13 @@ public class HW5 {
 		System.out.print("Oldest child of Sebastian: ");
 		tree5.oldestSon("Matthew"); 
 		System.out.print("Uncles of Dylan: ");
-		tree5.getUncles("Dylan");
+		tree5.uncles("Dylan");
 		System.out.print("Uncles of Sebastian: ");
-		tree5.getUncles("Sebastian");
-		System.out.print("Grandparent of Sebastian: ");
-		tree5.getGrandparent("Sebastian"); 
+		tree5.uncles("Sebastian");
+		System.out.print("\nGrandparent of Sebastian: ");
+		tree5.grandparent("Sebastian"); 
 		System.out.print("Uncles of Luke: ");
-		tree5.getUncles("Luke");
+		tree5.uncles("Luke");
 		
 	}
 	
@@ -77,9 +93,17 @@ public class HW5 {
 	public static void data(Node tree) throws Exception {
 		String filename = "Tree" + Integer.toString(num); 
 		Scanner sc = new Scanner(new File(filename));
+		StringBuilder title = new StringBuilder("   *** Data for " + filename + " ***");
+		String child;
+		String parent;
+		System.out.println(title);
 		while(sc.hasNext()) {
-			tree.insert(sc.next(), sc.next());
+			child = sc.next();
+			parent = sc.next();
+			tree.insert(child, parent);
+			System.out.println("Child: " + child + "\tParent: " + parent);
 		}
+		System.out.println();
 		num++;
 	}
 
